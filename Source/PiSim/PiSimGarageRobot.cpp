f5fadd67215731253cd57de10b2a889b98c7ea8b
Galvanicplane <erenarslan1910@gmail.com>
2026-08-20 15:56:10 +0300

    Fix undeclared bIsStructural identifier in PiSimGarageRobot.cpp

--- Source/PiSim/PiSimGarageRobot.cpp
+++ Source/PiSim/PiSimGarageRobot.cpp
@@ -688,6 +688,7 @@ void APiSimGarageRobot::BeginPlay()
 
                 FString MeshName = Sections[SecIdx].MeshName;
                 bool bIsCMOnly = MeshName.StartsWith(TEXT("CM_"), ESearchCase::IgnoreCase);
+                bool bIsStructural = bIsCMOnly || MeshName.StartsWith(TEXT("COG"), ESearchCase::IgnoreCase) || MeshName.StartsWith(TEXT("COL"), ESearchCase::IgnoreCase) || MeshName.StartsWith(TEXT("Cube"), ESearchCase::IgnoreCase) || MeshName.StartsWith(TEXT("Stick"), ESearchCase::IgnoreCase) || MeshName.StartsWith(TEXT("Buckett"), ESearchCase::IgnoreCase) || bLoadedDiskModel;
 
                 TArray<FVector2D> UV0;
                 TArray<FLinearColor> VertexColors;
